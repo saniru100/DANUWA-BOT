7ff135cad59c60aa0057ee523c002e310fe8b29a
saniru100 <saniyaman238@gmail.com>
2025-06-24 09:25:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ const defaultConfig = {
   ALIVE_IMG: "https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Danuwa%20-%20MD.png?raw=true",
   ALIVE_MSG: "*Hello👋 SANIRU-MD Is Alive Now😍*", // Change alive msg from here
   OPENAI_API_KEY: "",// Add OPENAI API key here
-  GEMINI_API_KEY: "", // Add Gemini API key here
+  GEMINI_API_KEY: "AIzaSyDJkbUK2FdDh9biIk5JZ4E-NiRIO9lRON8", // Add Gemini API key here
   REMOVE_BG_API_KEY:"", // Add removebg API key here
   WEATHER_API_KEY: "", // Add your weather API key here
   BOT_OWNER: "94776121326", // Replace your bot owner number here with 94(country code)
