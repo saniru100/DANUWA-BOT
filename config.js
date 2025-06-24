5f47e614b0a88c595ab9961c2e7374cfb6493bbd
saniru100 <saniyaman238@gmail.com>
2025-06-24 09:37:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ const defaultConfig = {
   ALIVE_MSG: "*Hello👋 SANIRU-MD Is Alive Now😍*", // Change alive msg from here
   OPENAI_API_KEY: "",// Add OPENAI API key here
   GEMINI_API_KEY: "AIzaSyDJkbUK2FdDh9biIk5JZ4E-NiRIO9lRON8", // Add Gemini API key here
-  REMOVE_BG_API_KEY:"", // Add removebg API key here
+  REMOVE_BG_API_KEY:"sZtXEBqDjxYyquCjvph8tuRJ", // Add removebg API key here
   WEATHER_API_KEY: "3a08023c63ba4a8782d40416252406", // Add your weather API key here
   BOT_OWNER: "94776121326", // Replace your bot owner number here with 94(country code)
   ownerNumber: ["94776121326"], // Replace your bot owner number here (same as bot owner number)
